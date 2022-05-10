a6ede91300c0acb7a3b6d131de58a75a6754560d
Florin <97301122+Florin67@users.noreply.github.com>
2022-05-10 16:39:11 +0300

    Add files via upload

---
+++ jsF/end.js
@@ -0,0 +1,5 @@
+const saveScoreBtn = document.querySelector('#saveScoreBtn')
+const finalScore = document.querySelector('#finalScore')
+const scoreText = document.querySelector('#score');
+
+scoreText.innerText = score
